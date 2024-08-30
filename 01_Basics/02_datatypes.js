18180fd66a5dbdad2f5e3d7c2910f5353179457f
Nityanand <127835667+nityanand22@users.noreply.github.com>
2024-08-30 01:25:34 +0000

    datatyped

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,29 @@
+// const a = "nityanand"
+// let b = 25
+// var c = true
+
+// // a = "bhoomi"
+// b = 45
+// var c = false
+
+// console.log(a, b, c);
+
+
+
+"use  strict" //treat all JS code as a newer version
+// alert("hello") // error -> alert is not defined because we are using nodejs not browser
+console.log(3+3);
+
+
+// Number => range 2 tp power 53
+// Bigint
+// String => ""
+// Boolean => true/false
+// null => standalone value
+// undefined => 
+// symbole => unique
+// objects
+
+// console.log(typeof "nitya")  // string
+console.log(typeof null)  // object
+console.log(typeof undefined)   // undefined
