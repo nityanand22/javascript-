8ed9ecd93cc2b618e5c946d85c89409507780024
Nityanand <127835667+nityanand22@users.noreply.github.com>
2024-08-30 01:45:01 +0000

    conversion of datatypes

---
+++ 01_Basics/03_ConversionOperation.js
@@ -0,0 +1,32 @@
+// let score = "66abc"
+let score = null
+
+// console.log(typeof score)    // number
+// console.log(typeof (score)) // number
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+
+// "33" => 33
+//"33abd" => NaN
+// true => 1   | false => 0
+
+
+// let islogged = 1
+// let islogged = ""
+// let islogged = "nityanand"
+let islogged = 0
+let booleanvalue = Boolean(islogged)
+// console.log(booleanvalue);  // true
+// console.log(booleanvalue);  // false
+// console.log(booleanvalue);  // true
+console.log(booleanvalue);  // false
+
+
+let num = 55
+let stringNumber = String(num)
+console.log(stringNumber);
+console.log(typeof stringNumber);
