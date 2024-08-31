941e4296b2646c2cb6647b25f20bceb3d36e3b6f
Nityanand <127835667+nityanand22@users.noreply.github.com>
2024-08-31 17:00:34 +0000

    operations added

--- 01_Basics/03_ConversionOperation.js
+++ 01_Basics/03_ConversionOperation.js
@@ -6,7 +6,7 @@ let score = null
 
 let valueInNumber = Number(score)
 // console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(valueInNumber);
 
 
 
@@ -23,10 +23,45 @@ let booleanvalue = Boolean(islogged)
 // console.log(booleanvalue);  // true
 // console.log(booleanvalue);  // false
 // console.log(booleanvalue);  // true
-console.log(booleanvalue);  // false
+// console.log(booleanvalue);  // false
 
 
 let num = 55
 let stringNumber = String(num)
-console.log(stringNumber);
-console.log(typeof stringNumber);  // string
+// console.log(stringNumber);
+// console.log(typeof stringNumber);  // string
+
+
+
+//************************Operation*******************
+let value = 3
+let negValue = -value
+// console.log(negValue)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**2)
+// console.log(2%2)
+
+let str1 = "hello"
+let str2 = " javascript"
+let str3 = str1 + str2
+// console.log(str3)
+// console.log(str1 + str2)
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+console.log(+true)
+console.log(+"")
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+// console.log(num1, num2, num3)
+
+let gameCounter = 100
+// gameCounter++
+++gameCounter
+console.log(gameCounter)
