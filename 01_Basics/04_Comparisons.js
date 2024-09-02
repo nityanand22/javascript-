e474ec7f49c515c3a8507dcfd1f86605a8c4d35e
Nityanand <127835667+nityanand22@users.noreply.github.com>
2024-09-02 02:32:36 +0000

    strict comparison

--- 01_Basics/04_Comparisons.js
+++ 01_Basics/04_Comparisons.js
@@ -4,14 +4,20 @@
 // console.log(2 <= 1)
 // console.log(2 != 1)
 
-console.log("2" > 1)
-console.log("82" > 1)
+// console.log("2" > 1)  // true
+// console.log("82" > 1)   // true
 
-console.log(null > 0)
-console.log(null == 0)
-console.log(null >= 0)
+// console.log(null > 0)  // false
+// console.log(null == 0)   // false
+// console.log(null >= 0)    // true
 
 
-console.log(undefined > 0)
-console.log(undefined == 0)
-console.log(undefined >= 0)
+// console.log(undefined > 0)  // false
+// console.log(undefined == 0)   // false
+// console.log(undefined >= 0)   // false
+
+// Always avoid red dotted conversion . this is the inconsistency of language. this is not the clean code
+
+// === strictly check
+
+console.log("2" === 2)   // false
