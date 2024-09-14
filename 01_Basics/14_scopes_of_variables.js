fe196368bb8c49703d13c677ef2e9c8a9906eeb0
Nityanand <127835667+nityanand22@users.noreply.github.com>
2024-09-14 06:39:27 +0000

    variables and  their scopes

---
+++ 01_Basics/14_scopes_of_variables.js
@@ -0,0 +1,15 @@
+// var c = 300
+let a = 300
+
+if(true){
+  let a = 10
+  const b = 20
+  // var c = 30
+  console.log(a)
+}
+
+// console.log(a)  // error => not defined
+// console.log(b)  // error => not defined
+// console.log(c)  // 30
+
+console.log(a)  // 300
