08724b1707ec24bd6bffa484e2d9e5feb950d399
Nityanand <127835667+nityanand22@users.noreply.github.com>
2024-09-23 13:44:21 +0530

    while and do while loops

---
+++ 01_Basics/21_while_and_doWhile_loop.js
@@ -0,0 +1,20 @@
+// let i = 0;
+// while(i <= 10){
+//   console.log(i)
+//   i++
+// }
+
+
+
+// let myArray = ["flash" , "batman" , "shaktiman"]
+// let i = 0;
+// while(i < myArray.length){
+//   console.log(myArray[i])
+//   i++;
+// }
+
+let score = 1;
+do {
+  console.log(score)
+  score++
+} while (score <= 10);
